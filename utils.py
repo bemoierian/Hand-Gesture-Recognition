6741c3dfff835d2231ed284becdc16c3914b989e
Bemoi Erian <bemoi.erian@gmail.com>
2023-05-13 22:29:20 +0300

    adjusted exposure

--- utils.py
+++ utils.py
@@ -73,11 +73,13 @@ class Utils:
         IHat = np.maximum(c[:,:, 1], c[:,:, 2])
         e = I - IHat
         fskin = np.logical_and(e >= 0.02711, e <= 0.1177)
+        # print(f"fskin shape {fskin.shape}")
         return fskin.astype(int)
 
 
     @staticmethod
     def getMaskedHand(frame):
+        # ------------Thresholding method 1-----------------
         # Convert image to HSV
         hsvim = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         # Lower boundary of skin color in HSV
@@ -89,14 +91,18 @@ class Utils:
 
         # Gaussian filter (blur) to remove noise
         skinMask = cv.GaussianBlur(skinMask, (17, 17), 0)
+        # print(f"skinMask shape {skinMask.shape}")
+        # print(f"skin mask: {skinMask}")
 
-
+        # ------------Thresholding method 2-----------------
         # threshImg = Utils.skin_color_thresholding(frame)
-        # threshImg = cv.normalize(threshImg, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
+        # threshImg = cv.normalize(threshImg, None, alpha=0, beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8U)
+        # print(f"threshImg shape {threshImg.shape}")
+        # print(f"threshImg: {threshImg}")
 
 
         greyImg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        # greyImg = cv.normalize(greyImg, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
+        # print(f"greyImg shape {greyImg.shape}")
         masked = cv.bitwise_and(greyImg, greyImg, mask=skinMask)
         # maskedBlurred = cv.GaussianBlur(masked, (41, 41), 0)
 
@@ -129,3 +135,21 @@ class Utils:
         except:
             k = 0
         return k
+    @staticmethod
+    def adjust_image(image):
+        # Convert image to LAB color space
+        lab = cv.cvtColor(image, cv.COLOR_BGR2LAB)
+
+        # Split the LAB image into different channels
+        l, a, b = cv.split(lab)
+
+        # Apply CLAHE (Contrast Limited Adaptive Histogram Equalization) to the L channel
+        clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(32,32))
+        cl = clahe.apply(l)
+
+        # Merge the CLAHE enhanced L channel with the original A and B channels
+        limg = cv.merge((cl,a,b))
+
+        # Convert the LAB image back to BGR color space
+        final = cv.cvtColor(limg, cv.COLOR_LAB2BGR)
+        return final
